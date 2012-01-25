deeb89b72dd784de1d878d70677d7677bf634c17
Reno Bowen <renobowen@gmail.com>
2012-01-24 19:04:41 -0800

    Initial commit of management interface
    Committing work completed prior to set up of git repository.
    
    *Basic management interface including:
    **View your data
    **Expand upon experiments->sessions, sessions->epochs
    **Edit experiment description
    *Added more efficient table column sorting
    *Preparing for inclusion of snazzier jquery ui functionality - drag and drop
    rows, etc

--- nimsgears/controllers/data.py
+++ nimsgears/controllers/data.py
@@ -15,6 +15,9 @@ from nimsgears.lib.base import BaseController
 
 import nimsutil
 
+import json # return raw json to browser in cases of database queries
+import transaction
+
 __all__ = ['DataController', 'PublicDataController']
 
 
@@ -28,7 +31,6 @@ class NimsAdminController(AdminController):
 
 class DataController(BaseController):
 
-    @expose()
     def index(self):
         redirect('/pub/browse')
 
@@ -127,10 +129,88 @@ class AuthDataController(DataController):
         dataset_cnt = len(Session.query.all())
         return dict(page='search', dataset_cnt=dataset_cnt)
 
-    @expose('nimsgears.templates.manage')
-    def manage(self):
-        return dict(page='manage', params={})
-
     @expose('nimsgears.templates.admin')
     def admin(self):
         return dict(page='admin', params={})
+
+    @expose()
+    def session_query(self, **kwargs):
+        """ Queries DB given info found in POST, TODO perhaps verify access level another time here??
+        """
+        def summarize_sess(session):
+            return (session.id, session.mri_exam, session.subject.lastname, session.subject.firstname)
+
+        try:
+            exp_id = int(kwargs['id'])
+        except:
+            exp_id = -1
+
+        sess_list = ([summarize_sess(item.Session) for item in
+            DBSession.query(Session, Experiment).join('experiment').filter(Experiment.id == exp_id).all()])
+
+        return json.dumps(sess_list)
+
+    @expose()
+    def update_epoch(self, **kwargs):
+        user = request.identity['user']
+
+        try:
+            epoch_id = int(kwargs['id'])
+            desc = kwargs['desc']
+        except:
+            epoch_id = -1
+            desc = None
+
+        rw_privilege = AccessPrivilege.query.filter_by(name = u'rw').first()
+        epoch = DBSession.query(Epoch).join('session', 'experiment', 'access', 'privilege').filter(Epoch.id == epoch_id).filter(Access.user == user).filter(AccessPrivilege.value > rw_privilege.value).first()
+
+        if epoch:
+            epoch.mri_desc = desc
+            transaction.commit()
+            success = True
+        else:
+            success = False
+
+        return json.dumps({'success':success})
+
+    @expose()
+    def epoch_query(self, **kwargs):
+        """ Queries DB given info found in POST, TODO perhaps verify access level another time here??
+        """
+        def summarize_epoch(epoch, sess_id):
+            return (epoch.id, epoch.mri_series, epoch.mri_acq, epoch.mri_desc, sess_id)
+
+        try:
+            sess_id = int(kwargs['id'])
+        except:
+            sess_id = -1
+
+        epoch_list = ([summarize_epoch(item.Epoch, sess_id) for item in
+            DBSession.query(Epoch, Session).join('session').filter(Session.id == sess_id).all()])
+
+        return json.dumps(epoch_list)
+
+    @expose('nimsgears.templates.manage')
+    def manage(self):
+        user = request.identity['user']
+
+        def summarize_exp(experiment, access):
+            return (experiment.id, experiment.owner.name, experiment.name, access)
+
+        exp_list = []
+        exp_list.extend([summarize_exp(item.Experiment, 'pi') for item in
+            DBSession.query(Experiment, ResearchGroup).join(ResearchGroup).filter(ResearchGroup.pis.contains(user)).all()])
+        access_levels = ['mg', 'rw']
+        for access_level in access_levels:
+            privilege = AccessPrivilege.query.filter_by(name=access_level).one()
+            exp_list.extend([summarize_exp(item.Experiment, privilege.description) for item in
+                DBSession.query(Experiment, Access).join(Access).filter(Access.user == user).filter(Access.privilege == privilege).all()])
+
+        columns = ['Owner', 'Name', 'Access']
+        session_columns = ['MRI Exam', 'Subject Last Name', 'Subject First Name']
+        epoch_columns = ['Series #', 'Acquisition #', 'Description', 'Session ID']
+        epoch_columns_flags = [0, 0, 1, 0]
+
+        access_levels.insert(0, 'pi')
+        return dict(page='manage', exp_list=exp_list, access_levels=access_levels, columns=columns, session_columns=session_columns, epoch_columns=epoch_columns, epoch_columns_flags=epoch_columns_flags)
+
---
+++ nimsgears/public/javascript/manage.js
@@ -0,0 +1,164 @@
+function makeSessionRow(sessionTuple)
+{
+    var sessionRow = document.createElement('tr');
+    var sessionCell;
+    sessionRow.id = 'sess_' + sessionTuple[0];
+    for (var i = 1; i < sessionTuple.length; i++) {
+        sessionCell = document.createElement('td');
+        sessionCell.textContent = sessionTuple[i];
+        sessionRow.appendChild(sessionCell);
+    }
+    sessionCell = document.createElement('td');
+    var span = document.createElement('span');
+    span.className = 'sess_detail';
+    span.textContent = ' >> ';
+    sessionCell.appendChild(span);
+    sessionRow.appendChild(sessionCell);
+    return sessionRow;
+};
+
+function compare(a, b) {
+   if (a < b)
+      return -1;
+   else if (a == b)
+      return 0;
+   else
+      return 1;
+}
+
+function sortTable(element, direction) {
+    indexOfColumn = element.closest('thead').find('th').index(element);
+    rows = element.closest('table').find('tbody tr');
+    sorted_rows = rows.sort(function mysort(a, b) {
+        return -direction * compare($($(a).find('td')[indexOfColumn]).text(), $($(b).find('td')[indexOfColumn]).text()); });
+    sorted_rows.slice(1).each(function() {
+        this.parentNode.insertBefore(this, sorted_rows[sorted_rows.index(this) - 1]); });
+};
+
+function blurOnEnter(event)
+{
+    var chCode = ('charCode' in event) ? event.charCode : event.keyCode;
+    if (chCode == 13) {
+        $(this).blur();
+    };
+};
+
+function setupDraggable(source, target) {
+    source.draggable({
+        helper: function(event) {
+            return $('<div style="background-color:red";>FUCK YOU</div>');
+        },
+    });
+};
+
+function setupDroppable(source, target) {
+    target.droppable({
+        hoverClass: 'bloop',
+        drop: function(event, ui) {
+            alert('row dropped ' + $(this).text());
+        },
+        accept: source.selector
+    });
+};
+
+function makeEpochRow(epochTuple)
+{
+    var epochRow = document.createElement('tr');
+    var epochCell;
+    epochRow.id = 'epoch_' + epochTuple[0];
+    for (var i = 1; i < epochTuple.length; i++) {
+        epochCell = document.createElement('td');
+        if (epoch_columns_flags[i-1] == 1) {
+          epochInput = document.createElement('input');
+          epochInput.value = epochTuple[i];
+          // put this type of stuff into the CSS file eventually (with a class perhaps)
+          epochInput.readOnly = true;
+          epochInput.style.border = 'none';
+          epochInput.style.background = 'transparent';
+          epochInput.onkeypress = blurOnEnter;
+          epochInput.onblur = function()
+          {
+            inputObject = $(this);
+            // verify they've changed the value since clicking it
+            if (inputObject.val() != inputObject.data('cache')) {
+                epoch_id = inputObject.parents('tr').attr('id').split('_')[1];
+                inputObject.attr('readonly', true);
+                $.ajax({
+                    type: 'POST',
+                    url: "update_epoch",
+                    dataType: "json",
+                    data: {
+                        id: epoch_id,
+                        desc: inputObject.val()
+                    },
+                    success: function(data) {
+                        if (!data.success) {
+                            inputObject.val(inputObject.data('cache'));
+                            alert("Change failed to commit.");
+                        }
+                    },
+                }); // ajax call
+            }
+          };
+          epochCell.onclick = function()
+          {
+              input = $(this).children('input');
+              input.data('cache', input.val());
+              input.attr('readonly', false);
+          };
+          epochCell.appendChild(epochInput);
+        } else {
+          epochCell.textContent = epochTuple[i];
+        }
+        epochRow.appendChild(epochCell);
+    }
+    return epochRow;
+};
+
+function refreshEpochList()
+{
+    var sess_id = $(this).parents('tr').attr('id').split('_')[1];
+    $.ajax({
+        type: 'POST',
+        url: "epoch_query",
+        dataType: "json",
+        data: {
+            id: sess_id
+        },
+        success: function(data) {
+            var table_body = $("#epochs").children('tbody');
+            table_body.children().remove();
+            for (var i = 0; i < data.length; i++) {
+                table_body.append(makeEpochRow(data[i]));
+            }
+        },
+    }); // ajax call
+};
+
+function refreshSessionList()
+{
+    var exp_id = $(this).parents('tr').attr('id').split('_')[1];
+    $.ajax({
+        type: 'POST',
+        url: "session_query",
+        dataType: "json",
+        data: {
+            id: exp_id
+        },
+        success: function(data) {
+            $("#epochs").children('tbody').children().remove(); // clean up epoch table
+            var table_body = $("#sessions").children('tbody');
+            table_body.children().remove(); // clean up session table
+            for (var i = 0; i < data.length; i++) { // repopulate session table
+                table_body.append(makeSessionRow(data[i]));
+            }
+            $(".sess_detail").click(refreshEpochList); // set callbacks on new expand buttons for epoch table
+            //setupDraggable($("#t2 tr"), $("#t1 tr"));
+        },
+    }); // ajax call
+};
+
+function setupCallbacks()
+{
+    $(".exp_detail").click(refreshSessionList);
+};
