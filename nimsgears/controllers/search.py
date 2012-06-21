77fc2200e9ef120e6be75471bad585c9614a445f
Bob Dougherty <bobd@stanford.edu>
2012-06-20 17:28:37 -0700

    Added more search functionality.

--- nimsgears/controllers/search.py
+++ nimsgears/controllers/search.py
@@ -5,6 +5,9 @@ from tg import expose, request
 from repoze.what import predicates
 import transaction
 
+import re
+import datetime
+
 from nimsgears.model import *
 from nimsgears.controllers.nims import NimsController
 
@@ -16,7 +19,7 @@ class SearchController(NimsController):
     @expose('nimsgears.templates.search')
     def index(self):
         dataset_cnt = Session.query.count()
-        param_list = ['Subject Name', 'PSD Name']
+        param_list = ['Subject Name', 'Subject Age', 'PSD Name', 'Exam', 'Operator']
         epoch_columns = [('Access', 'col_access'), ('Group', 'col_sunet'), ('Experiment', 'col_exp'), ('Date & Time', 'col_datetime'), ('Subj. Code', 'col_subj'), ('Description', 'col_desc')]
         dataset_columns = [('Data Type', 'col_type')]
         return dict(page='search',
@@ -27,12 +30,14 @@ class SearchController(NimsController):
 
     @expose()
     def query(self, **kwargs):
+        # For more robust search query parsing, check out pyparsing.
         result = {}
-        if 'search_param' in kwargs and 'search_query' in kwargs:
-            db_query = None
+        if 'search_param' in kwargs and 'search_query' in kwargs and 'date_from' in kwargs and 'date_to' in kwargs:
             search_query = kwargs['search_query'].replace('*', '%')
+            if search_query==None:
+                search_query = "*"
+            db_query = DBSession.query(Epoch, Session, Subject, Experiment)
             if kwargs['search_param'] == 'PSD Name':
-                db_query = DBSession.query(Epoch, Session, Subject, Experiment)
                 db_query = (db_query
                     .join(Session, Epoch.session)
                     .join(Subject, Session.subject)
@@ -40,12 +45,51 @@ class SearchController(NimsController):
                     .join(Dataset, Epoch.datasets)
                     .filter(Dataset.psd.ilike(search_query)))
             elif kwargs['search_param'] == 'Subject Name':
-                db_query = DBSession.query(Epoch, Session, Subject, Experiment)
                 db_query = (db_query
                     .join(Session, Epoch.session)
                     .join(Subject, Session.subject)
                     .join(Experiment, Subject.experiment)
                     .filter(Subject.lastname.ilike(search_query)))
+            elif kwargs['search_param'] == 'Exam':
+                db_query = (db_query
+                    .join(Session, Epoch.session)
+                    .join(Subject, Session.subject)
+                    .join(Experiment, Subject.experiment)
+                    .filter(Session.exam == int(search_query)))
+            elif kwargs['search_param'] == 'Operator':
+                db_query = (db_query
+                    .join(Session, Epoch.session)
+                    .join(Subject, Session.subject)
+                    .join(Experiment, Subject.experiment)
+                    .join(User)
+                    .filter(User.uid.ilike(search_query)))
+            elif kwargs['search_param'] == 'Subject Age':
+                # TODO: allow more flexibility in age searches. E.g., "34", "34y", "408m", "=34", ">30", "<40", ">30y and <450m", "30 to 40", etc.
+                min_age = None
+                max_age = None
+                a = re.match(r"\s*>(\d+)\s*<(\d+)|\s*>(\d+)|\s*(\d+)\s*to\s*(\d+)|\s*>(\d+)|\s*(\d+)", search_query)
+                if a != None:
+                    min_age = max(a.groups()[0:1]+a.groups()[2:4])
+                    max_age = max(a.groups()[1:2]+a.groups()[4:5])
+                    if min_age==None and max_age==None:
+                        min_age = a.groups()[6]
+                        max_age = a.groups()[6]
+                if min_age==None:
+                    min_age = 0
+                if max_age==None:
+                    max_age = 999999999
+                db_query = (db_query
+                    .join(Session, Epoch.session)
+                    .join(Subject, Session.subject)
+                    .join(Experiment, Subject.experiment)
+                    .filter(Session.timestamp - Subject.dob >= datetime.timedelta(days=float(min_age)*365.25))
+                    .filter(Session.timestamp - Subject.dob <= datetime.timedelta(days=float(max_age)*365.25)))
+
+            if kwargs['date_from'] != None and re.match(r'\s*\d+\d+',kwargs['date_from'])!=None:
+                db_query = db_query.filter(Session.timestamp >= kwargs['date_from'])
+            if kwargs['date_to'] != None and re.match(r'\s*\d+\d+',kwargs['date_to'])!=None:
+                db_query = db_query.filter(Session.timestamp <= kwargs['date_to'])
+
             if db_query:
                 result['data'], result['attrs'] = self._process_result(db_query.all())
                 result['success'] = True
