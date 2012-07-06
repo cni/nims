48de37d7b48383e7ffafb35f33f0b40ba8ec8a26
Gunnar Schaefer <gsfr@stanford.edu>
2012-07-06 11:25:12 -0700

    sorter can now preserve unsortable files

--- nimsproc/sorter.py
+++ nimsproc/sorter.py
@@ -20,16 +20,18 @@ from nimsgears import model
 
 class Sorter(object):
 
-    def __init__(self, db_uri, stage_path, nims_path, dir_mode, sleep_time, log):
+    def __init__(self, db_uri, stage_path, unsort_path, nims_path, dir_mode, preserve_mode, sleep_time, log):
         super(Sorter, self).__init__()
         self.stage_path = stage_path
+        self.unsort_path = unsort_path
         self.nims_path = nims_path
         self.dir_mode = dir_mode
+        self.preserve_mode = preserve_mode
         self.sleep_time = sleep_time
         self.log = log
         self.alive = True
+        self.dataset_classes = sorted(model.PrimaryMRData.__subclasses__(), key=lambda cls: cls.priority)
         model.init_model(sqlalchemy.create_engine(db_uri))
-        self.mr_data_factory = MRDataFactory()
 
     def halt(self):
         self.alive = False
@@ -62,36 +64,35 @@ class Sorter(object):
 
     def sort_file(self, filepath):
         self.log.debug('Sorting %s' % os.path.basename(filepath))
-        dataset = self.mr_data_factory.dataset_at_path_for_file(self.nims_path, filepath)
+        dataset = self.dataset_at_path(self.nims_path, filepath)
         if dataset:
             ext = dataset.filename_ext if os.path.splitext(filepath)[1] != dataset.filename_ext else ''
             shutil.move(filepath, os.path.join(self.nims_path, dataset.relpath, os.path.basename(filepath) + ext))
             dataset.updatetime = datetime.datetime.now()
+            dataset.untrash()
             transaction.commit()
-        else:
-            os.remove(filepath)
+        elif self.preserve_mode:
+            unsort_path = nimsutil.make_joined_path(self.unsort_path, os.path.dirname(os.path.relpath(filepath, self.stage_path)))
+            shutil.move(filepath, unsort_path)
 
     def sort_directory(self, dirpath, filenames):
         self.log.debug('Sorting %s in directory mode' % os.path.basename(dirpath))
-        dataset = self.mr_data_factory.dataset_at_path_for_file(self.nims_path, os.path.join(dirpath, filenames[0]))
+        dataset = self.dataset_at_path(self.nims_path, os.path.join(dirpath, filenames[0]))
         if dataset:
             for filepath in [os.path.join(dirpath, filename) for filename in filenames]:
                 ext = dataset.filename_ext if os.path.splitext(filepath)[1] != dataset.filename_ext else ''
                 shutil.move(filepath, os.path.join(self.nims_path, dataset.relpath, os.path.basename(filepath) + ext))
             dataset.updatetime = datetime.datetime.now()
+            dataset.untrash()
             transaction.commit()
+        elif self.preserve_mode:
+            unsort_path = nimsutil.make_joined_path(self.unsort_path, os.path.dirname(os.path.relpath(dirpath, self.stage_path)))
+            shutil.move(dirpath, unsort_path)
 
-
-class MRDataFactory(object):
-
-    def __init__(self):
-        super(MRDataFactory, self).__init__()
-        self.dataset_classes = sorted(model.PrimaryMRData.__subclasses__(), key=lambda cls: cls.priority)
-
-    def dataset_at_path_for_file(self, nims_path, filepath):
+    def dataset_at_path(self, nims_path, filepath):
         """Return instance of appropriate MRIDataset subclass for provided file."""
         for dataset_class in self.dataset_classes:
-            dataset = dataset_class.at_path_for_file_and_datatype(nims_path, filepath)
+            dataset = dataset_class.at_path(nims_path, filepath)
             if dataset: break
         return dataset
 
@@ -104,6 +105,7 @@ class ArgumentParser(argparse.ArgumentParser):
         self.add_argument('stage_path', help='path to staging area')
         self.add_argument('nims_path', help='data destination')
         self.add_argument('-d', '--dirmode', action='store_true', help='assume files are pre-sorted by directory')
+        self.add_argument('-p', '--preserve', action='store_true', help='preserve unsortable files')
         self.add_argument('-s', '--sleeptime', type=int, default=10, help='time to sleep before checking for new files')
         self.add_argument('-n', '--logname', default=os.path.splitext(os.path.basename(__file__))[0], help='process name for log')
         self.add_argument('-f', '--logfile', help='path to log file')
@@ -115,9 +117,10 @@ if __name__ == '__main__':
 
     log = nimsutil.get_logger(args.logname, args.logfile, args.loglevel)
     stage_path = nimsutil.make_joined_path(args.stage_path, 'sort')
+    unsort_path = nimsutil.make_joined_path(args.stage_path, 'unsortable')
     nims_path = nimsutil.make_joined_path(args.nims_path)
 
-    sorter = Sorter(args.db_uri, stage_path, nims_path, args.dirmode, args.sleeptime, log)
+    sorter = Sorter(args.db_uri, stage_path, unsort_path, nims_path, args.dirmode, args.preserve, args.sleeptime, log)
 
     def term_handler(signum, stack):
         sorter.halt()
