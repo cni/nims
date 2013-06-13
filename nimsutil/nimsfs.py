80a20c7c861044eaea7435916398251d1e57d04a
Bob Dougherty <bobd@stanford.edu>
2013-06-12 17:02:01 -0700

    Refactored to use fusepy instead of python-fuse
    I had some issues getting python-fuse to play nicely with the garbage
    collection thread. After some investigation, I got the impression that
    the ctypes-based fusepy binding was cleaner and easier to work with than
    python-fuse. The codebase for fusepy is certainly simpler-- it's just
    one python file (fuse.py). So, I refactored to use it and got the
    grabage collection working. So not the caches will not get out of
    control. I also added a copy of fuse.py for convenience.

--- nimsutil/nimsfs.py
+++ nimsutil/nimsfs.py
@@ -12,63 +12,81 @@ is permitted on the system, then these requirements should be met.
 """
 
 import os, sys
-import errno  # for error number codes (ENOENT, etc) - note: these must be returned as negatives
+import errno  # for error number codes (ENOENT, etc)
 import stat   # for file properties
 import time
+import argparse
 import pwd    # to translate uid to username
 import grp    # to translate gid to groupname
 import fuse
-from fuse import Fuse
 
 import sqlalchemy
 from nimsgears.model import *
-db_uri = 'postgresql://nims:nims@nimsfs.stanford.edu:5432/nims'
-init_model(sqlalchemy.create_engine(db_uri))
 
-fuse.fuse_python_api = (0, 2)
-
-
+import threading
 import collections
 import functools
 
-class memoized(object):
+
+class memoize(object):
     '''Decorator to cache a function's return value each time it is called.
     If called later with the same arguments, the cached value is returned.
     '''
-    def __init__(self, func):
-       self.func = func
-       self.maxtime = 3
-       self.cache = {}
-       self.cachetime = {}
-    def __call__(self, *args):
-       if not isinstance(args, collections.Hashable):
-           # uncacheable. a list, for instance.
-           # better to not cache than blow up.
-           return self.func(*args)
-       if args in self.cache and time.time() - self.cachetime[args] < self.maxtime:
-           return self.cache[args]
-       else:
-           value = self.func(*args)
-           self.cache[args] = value
-           self.cachetime[args] = time.time()
-           return value
-    def __repr__(self):
-       '''Return the function's docstring.'''
-       return self.func.__doc__
-    def __get__(self, obj, objtype):
-       '''Support instance methods.'''
-       return functools.partial(self.__call__, obj)
+    def __init__(self, cachetime=5):
+        # If there are decorator arguments, the decorated function is not passed to the constructor.
+        # Instead, it's passed to __call__, which will get called only once upon decoration.
+        self.cachetime = cachetime
+        self.cache = {}
+        self.lock = threading.Lock()
+        self.garbage_collector = threading.Thread(target=self.collect_garbage)
+        self.garbage_collector.daemon = True
+        self.garbage_collector.start()
 
-@memoized
-def get_groups(user):
+    def collect_garbage(self):
+        while True:
+            now = time.time()
+            with self.lock:
+                for key in self.cache.keys():
+                    if self.cache[key][1] + self.cachetime < now:
+                        del self.cache[key]
+            time.sleep(self.cachetime * 2)
+
+    def __call__(self, func):
+        def wrapped_func(*args):
+            # Ensure the args are hashable. If not, don't try to cache.
+            if not isinstance(args, collections.Hashable):
+                return func(*args)
+            # Need to lock to keep the garbage collector from deleting something as we read it.
+            with self.lock:
+                if args in self.cache and self.cache[args][1] + self.cachetime > time.time():
+                    value = self.cache[args][0]
+                else:
+                    value = func(*args)
+                    self.cache[args] = (value,time.time())
+            return value
+        return wrapped_func
+
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
+
+@memoize()
+def get_groups(username):
+    user = User.get_by(uid=unicode(username))
     experiments = (Experiment.query.join(Access)
                    .filter(Access.user==user)
                    .filter(Access.privilege>=AccessPrivilege.value(u'Anon-Read'))
                    .all())
     return sorted(set([e.owner.gid.encode() for e in experiments]))
 
-@memoized
-def get_experiments(user, group_name):
+
+@memoize()
+def get_experiments(username, group_name):
+    user = User.get_by(uid=unicode(username))
     experiments = (Experiment.query.join(Access)
                    .join(ResearchGroup, Experiment.owner)
                    .filter(ResearchGroup.gid.ilike(unicode(group_name)))
@@ -77,8 +95,9 @@ def get_experiments(user, group_name):
                    .all())
     return sorted([e.name.encode() for e in experiments])
 
-@memoized
-def get_sessions(user, group_name, exp_name):
+@memoize()
+def get_sessions(username, group_name, exp_name):
+    user = User.get_by(uid=unicode(username))
     sessions = (Session.query
                 .join(Subject, Session.subject)
                 .join(Experiment, Subject.experiment)
@@ -90,9 +109,10 @@ def get_sessions(user, group_name, exp_name):
                 .all())
     return sorted([s.name.encode() for s in sessions])
 
-@memoized
-def get_epochs(user, group_name, exp_name, session_name):
+@memoize()
+def get_epochs(username, group_name, exp_name, session_name):
     # FIXME: we should explicitly set the session name so that we can be sure the exam is there.
+    user = User.get_by(uid=unicode(username))
     sp = session_name.split('_')
     if len(sp)>2 or '%' in sp[0]:
         q = (Epoch.query
@@ -112,8 +132,9 @@ def get_epochs(user, group_name, exp_name, session_name):
         epoch_names = []
     return epoch_names
 
-@memoized
-def get_datasets(user, group_name, exp_name, session_name, epoch_name):
+@memoize()
+def get_datasets(username, group_name, exp_name, session_name, epoch_name, datapath):
+    user = User.get_by(uid=unicode(username))
     # FIXME: we should explicitly set the epoch name
     ssp = session_name.split('_')
     if len(ssp)>2:
@@ -142,149 +163,128 @@ def get_datasets(user, group_name, exp_name, session_name, epoch_name):
                      .filter((Access.privilege >= AccessPrivilege.value(u'Read-Only')) | ((Dataset.kind != u'primary') & (Dataset.kind != u'secondary')))
                      .all())
         if '%' in epoch_name:
-            print 'DATASETS: %d' % len(datasets)
             # return a flat structure with legacy-style filenames
             datafiles = []
             for d in datasets:
                 for f in d.filenames:
                     display_name = '%04d_%02d_%s.%s' % (d.container.series, d.container.acq, d.container.description, '.'.join(f.split('.')[1:]))
-                    datafiles.append((display_name.encode(), os.path.join(server.datapath,d.relpath,f).encode()))
+                    datafiles.append((display_name.encode(), os.path.join(datapath,d.relpath,f).encode()))
         else:
             # Use the filename on disk
-            datafiles = [(f.encode(), os.path.join(server.datapath,d.relpath,f).encode()) for d in datasets for f in d.filenames]
+            datafiles = [(f.encode(), os.path.join(datapath,d.relpath,f).encode()) for d in datasets for f in d.filenames]
     else:
         datafiles = []
     return datafiles
 
 
-class NimsfsStat(fuse.Stat):
-    """
-    Class for Stat objects.
-    """
-    def __init__(self, is_dir, size, uid, gid, timestamp=None):
-        fuse.Stat.__init__(self)
-        if not timestamp:
-            timestamp = int(time.time())
-        if is_dir:
-            self.st_mode = stat.S_IFDIR | 0555
-            self.st_nlink = 2
-        else:
-            self.st_mode = stat.S_IFREG | 0444
-            self.st_nlink = 1
-            self.st_size = size
-        self.st_atime = timestamp
-        self.st_mtime = timestamp
-        self.st_ctime = timestamp
-        self.st_uid = uid
-        self.st_gid = gid
+class Nimsfs(fuse.LoggingMixIn, fuse.Operations):
 
-
-class Nimsfs(Fuse):
-
-    def __init__(self, *args, **kw):
-        Fuse.__init__(self, *args, **kw)
-        self.datapath = '/_nimsfs'
+    def __init__(self, datapath, db_uri):
+        self.datapath = datapath
+        self.rwlock = threading.Lock()
+        self.db_uri = 'postgresql://nims:nims@nimsfs.stanford.edu:5432/nims'
+        init_model(sqlalchemy.create_engine(self.db_uri))
+        # not sure if it's a good idea to store these here. Is there a separate instance of this object for each
+        # call to the fs? If so, then this is ok. But if this instance is shared, then we can't store specific info here.
         self.fp = None
         self.file_size = 0
+        self.file_ts = 0
 
-    def getattr(self, path):
-        #print 'GETATTR: path=' + path
+    def getattr(self, path, fh=None):
+        uid, gid, pid = fuse.fuse_get_context()
         fn = path.split('/')[-1]
         is_dir = '%' in fn or not bool(os.path.splitext(fn)[1])
-        size = 0
-        if not is_dir:
-            context = self.GetContext()
-            username = pwd.getpwuid(context['uid']).pw_name
-            groupname = grp.getgrgid(context['gid']).gr_name
-            user = User.get_by(uid=unicode(username))
+        ts = int(time.time())
+        if is_dir:
+            size = 0
+            mode = stat.S_IFDIR | 0555
+            nlink = 2
+        else:
+            mode = stat.S_IFREG | 0774
+            nlink = 1
+            username = pwd.getpwuid(uid).pw_name
+            groupname = grp.getgrgid(gid).gr_name
             cur_path = path.split('/')
             if len(cur_path) == 6:
-                files = get_datasets(user, cur_path[1], cur_path[2], cur_path[3], cur_path[4])
+                files = get_datasets(username, cur_path[1], cur_path[2], cur_path[3], cur_path[4], self.datapath)
                 fname = next((f[1] for f in files if f[0] == cur_path[5]), None)
                 if fname:
                     size = os.path.getsize(fname)
+                    ts = os.path.getmtime(fname)
                 else:
-                    return -errno.ENOENT
-        return NimsfsStat(is_dir, size, 0, 0)
+                    raise fuse.FuseOSError(errno.ENOENT)
+        return {'st_atime':ts, 'st_ctime':ts, 'st_gid':gid, 'st_mode':mode, 'st_mtime':ts, 'st_nlink':nlink, 'st_size':size, 'st_uid':uid}
 
-    def readdir(self, path, offset):
-        #print 'READDIR: path=' + path
-        context = self.GetContext()
-        username = pwd.getpwuid(context['uid']).pw_name
-        groupname = grp.getgrgid(context['gid']).gr_name
-        user = User.get_by(uid=unicode(username))
+    def readdir(self, path, fh):
+        uid, gid, pid = fuse.fuse_get_context()
+        username = pwd.getpwuid(uid).pw_name
+        groupname = grp.getgrgid(gid).gr_name
         cur_path = path.split('/')
         if len(cur_path) < 2 or not cur_path[1]:
-            dirs = get_groups(user)
+            dirs = get_groups(username)
         elif len(cur_path) < 3:
-            dirs = get_experiments(user, cur_path[1])
+            dirs = get_experiments(username, cur_path[1])
         elif len(cur_path) < 4:
-            dirs = get_sessions(user, cur_path[1], cur_path[2])
+            dirs = get_sessions(username, cur_path[1], cur_path[2])
         elif len(cur_path) < 5:
-            dirs = get_epochs(user, cur_path[1], cur_path[2], cur_path[3])
+            dirs = get_epochs(username, cur_path[1], cur_path[2], cur_path[3])
         elif len(cur_path) == 5:
-            dirs = [d[0] for d in get_datasets(user, cur_path[1], cur_path[2], cur_path[3], cur_path[4])]
+            dirs = [d[0] for d in get_datasets(username, cur_path[1], cur_path[2], cur_path[3], cur_path[4], self.datapath)]
         else:
             dirs = []
-        for e in ['.','..'] + dirs:
-            yield fuse.Direntry(e)
+        return ['.','..'] + dirs
 
     def open(self, path, flags):
-        # Only support for 'READ ONLY' flag
+        # Only support 'READ ONLY' flag
         access_flags = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
+        fh = 0
         if flags & access_flags != os.O_RDONLY:
-            return -errno.EACCES
+            raise fuse.FuseOSError(errno.EACCES)
         else:
-            context = self.GetContext()
-            username = pwd.getpwuid(context['uid']).pw_name
-            groupname = grp.getgrgid(context['gid']).gr_name
-            user = User.get_by(uid=unicode(username))
             cur_path = path.split('/')
             if len(cur_path) == 6:
-                files = get_datasets(user, cur_path[1], cur_path[2], cur_path[3], cur_path[4])
+                uid, gid, pid = fuse.fuse_get_context()
+                username = pwd.getpwuid(uid).pw_name
+                groupname = grp.getgrgid(gid).gr_name
+                files = get_datasets(username, cur_path[1], cur_path[2], cur_path[3], cur_path[4], self.datapath)
                 fname = next((f[1] for f in files if f[0] == cur_path[5]), None)
                 if fname:
                     self.file_size = os.path.getsize(fname)
-                    self.fp = open(fname, 'rb')
-            return 0
+                    self.file_ts = os.path.getmtime(fname)
+                    fh = os.open(fname, flags)
+                else:
+                    raise fuse.FuseOSError(errno.ENOENT)
+        return fh
 
-    def read(self, path, size, offset):
-        if self.fp:
-            self.fp.seek(offset)
-            buf = self.fp.read(size)
-        else:
-            buf = ''
-        return buf
-
-    def flush(self, fh=None):
-        if self.fp:
-            self.fp.close()
+    def read(self, path, size, offset, fh):
+        with self.rwlock:
+            os.lseek(fh, offset, 0)
+            return os.read(fh, size)
 
     def fgetattr(self, fh=None):
-        return NimsfsStat(0, self.file_size, 0, 0)
+        uid, gid, pid = fuse.fuse_get_context()
+        mode = stat.S_IFREG | 0444
+        return {'st_atime':self.file_ts, 'st_ctime':self.file_ts, 'st_gid':gid, 'st_mode':mode, 'st_mtime':self.file_ts, 'st_nlink':1, 'st_size':self.file_size, 'st_uid':uid}
+
+    def flush(self, path, fh):
+        os.fsync(fh)
+
+class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self):
+        super(ArgumentParser, self).__init__()
+        self.description = """Mount a NIMS filesystem. This exposes the NIMS file structure as a reqular filesystem using fuse."""
+        self.add_argument('-n', '--no_allow_other', action='store_true', help='Use this flag to disable the "allow_other" option. (For normal use, be sure to enable allow_other in /etc/fuse.conf)')
+        self.add_argument('-d', '--debug', action='store_true', help='Start the filesystem in debug mode')
+        uri = 'postgresql://nims:nims@nimsfs.stanford.edu:5432/nims'
+        self.add_argument('-u', '--uri', metavar='URI', default=uri, help='URI pointing to the NIMS database. (Default=%s)' % uri)
+        self.add_argument('datapath', help='path to NIMS data')
+        self.add_argument('mountpoint', help='mountpoint for NIMSfs')
 
 
 if __name__ == '__main__':
-    usage = ('nimsfs.py [mountpoint]\n'
-            +'Mount the NIMS virtual filesystem tree at the specified mountpoint.\n'
-            +'To unmount, use fusermount -u [mountpoint].\n\n'
-            +Fuse.fusage)
-
-    server = Nimsfs(version='%prog ' + fuse.__version__, usage=usage, dash_s_do='setsingle')
-
-    # To use multithreading, we'd need to protect all methods of
-    # NimsfsFile class with locks to prevent race conditions
-    server.multithreaded = False
-
-    server.parser.add_option(mountopt='datapath', metavar='PATH', default=server.datapath,
-                             help="nims data path. [default=%default]")
-    server.parse(values=server, errex=1)
-
-    try:
-        server.fuse_args.mount_expected()
-    except OSError:
-        print >> sys.stderr, 'Error mounting NIMS.'
-        sys.exit(1)
-
-    server.main()
+    args = ArgumentParser().parse_args()
+    fuse = fuse.FUSE(Nimsfs(datapath=args.datapath, db_uri=args.uri),
+                     args.mountpoint,
+                     debug=args.debug,
+                     allow_other=(not args.no_allow_other))
 
