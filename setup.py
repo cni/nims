665fd775a7f2cde9a85303000078bf1d9858ce61
Gunnar Schaefer <gsfr@stanford.edu>
2013-08-01 11:35:48 -0700

    adding missing repoze dependencies

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ install_requires=[
     "sqlalchemy-migrate",
     "repoze.who",
     "repoze.who-friendlyform >= 1.0.4",
+    "repoze.what >= 1.0.8",
+    "repoze.what-quickstart",
     "tgext.admin >= 0.5.1",
     "repoze.who.plugins.sa",
     "tw2.forms",
