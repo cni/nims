a82ff56fa35687dbdaeb69c7c9d9d4b24bc87572
Sara Benito <sbenito@stanford.edu>
2013-09-12 14:20:11 -0700

    File upload working

--- nimsgears/controllers/upload.py
+++ nimsgears/controllers/upload.py
@@ -1,6 +1,9 @@
-
-from tg import expose
+import os
+from tg import expose, redirect
 from nimsgears.controllers.nims import NimsController
+from nimsdata.nimsdata import NIMSData
+from nimsdata.nimsdicom import NIMSDicom
+import json
 
 class UploadController(NimsController):
 
@@ -9,16 +12,38 @@ class UploadController(NimsController):
         return dict(page='upload')
 
     @expose()
-    def query(self, **kwargs):
-        result = {'success': False}
-        return json.dumps(result)
-
-    @expose()
     def submit(self, **kwargs):
-        print kwargs
-        # This method will be called when a file is being uploaded
-        return dict()
+        print '+++++++++++++++++++++++++++++++ args: ', kwargs
 
+#        for i in dir(request):
+#            print i, ' : ', request.__getattr__(i)
+        if 'files[]' in kwargs:
+            files = kwargs['files[]']
+            if not type(files) is list:
+                files = [files]
+
+            i = 0
+            for file in files:
+                print dir(file)
+                content = file.file.read()
+                name = '/tmp/test-%d.data' % i
+                out = open(name, 'w')
+                out.write(content)
+                out.close()
+
+                try:
+                    data = NIMSDicom(name)
+                    print file.filename, ': ', len(content)
+                    print '++++++++++++++ exam_uid: ', data.exam_uid
+                    print data.get_metadata()
+                except:
+                    print "Couldn't understand the file"
+                    return json.dumps({'processed' : False, 'message' : "File %s could not be parsed" % file.filename})
+
+                # This method will be called when a file is being uploaded
+                return json.dumps({'processed' : True, 'message' : "file processed ok"})
+        else:
+            return json.dumps({'processed': False, 'message': 'No file was selected'})
 
 
 
