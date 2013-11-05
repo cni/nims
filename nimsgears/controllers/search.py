0c3fb50216f7662396447fcb87e28edad810b219
Sara Benito <sbenito@stanford.edu>
2013-11-05 10:36:57 -0800

    Change compile to match in regex

--- nimsgears/controllers/search.py
+++ nimsgears/controllers/search.py
@@ -23,7 +23,7 @@ def is_ascii(s):
 
 def is_date(s):
     # date format: yyyy-mm-dd
-    if bool(re.compile(r'^(19|20|21)\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])').match(s)):
+    if bool(re.match(r'^(19|20|21)\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])', s)):
         return True
     else:
         return False
